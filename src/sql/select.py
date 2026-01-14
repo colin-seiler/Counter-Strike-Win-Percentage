2146555443bc668fe6b805a139c6149b3d3576fb
25hady <25hady@users.noreply.github.com>
2026-01-14 18:26:38 +0000

    updated count to return first value in tuple not whole tuple

--- src/sql/select.py
+++ src/sql/select.py
@@ -1,4 +1,4 @@
-from src.sql.statements import SELECT_BY_QUEUE, SELECT_BY_GAME_ID, SELECT_FAILED_SQL
+from src.sql.statements import SELECT_BY_QUEUE, SELECT_BY_GAME_ID
 
 def select_match(conn, game_id=None):
     if not game_id:
@@ -17,7 +17,7 @@ def select_match(conn, game_id=None):
 def check_pending_count(conn):
     cur = conn.cursor()
     cur.execute("""
-    SELECT COUNT(*) FROM match_queue WHERE status IS 'pending'
+    SELECT COUNT(*) FROM match_queue WHERE status = 'pending'
     """)
-    count = cur.fetchone()
+    count = cur.fetchone()[0]
     return count
