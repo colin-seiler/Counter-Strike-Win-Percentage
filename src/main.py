8bd193b443cc7be88bfd827c2cf17700987328ab
25hady <25hady@users.noreply.github.com>
2026-01-15 18:00:39 +0000

    add suppress pandas warnings

--- src/main.py
+++ src/main.py
@@ -1,8 +1,18 @@
+import time
+import random
+
 from src.sql.select import check_pending_count
 from src.sql.create import create_connection, create_table
 
 from src.workflow.queue import scrape_initial_matches, get_match_in_queue
 from src.workflow.process import process
+
+import warnings
+import pandas as pd
+
+warnings.filterwarnings('ignore', category=pd.errors.PerformanceWarning)
+warnings.filterwarnings('ignore', category=pd.errors.SettingWithCopyWarning)
+warnings.filterwarnings('ignore', category=FutureWarning)
 
 def main():
     conn = create_connection()
@@ -27,6 +37,11 @@ def main():
             process(conn, match_info, dem_files)
         count = check_pending_count(conn)
         print(f'Action completed: There are {count} matches left in queue')
+        
+        if count > 0:
+            sleep = random.uniform(20, 30)
+            print(f'Sleeping for {sleep:.1f} seconds')
+            time.sleep(sleep)
 
 if __name__ == "__main__":
     main()
